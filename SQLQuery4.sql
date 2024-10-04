de89b33d2bae2db4321ce5c6ee5a409193ee6e25
Bruna Barboza <165702836+brubsb@users.noreply.github.com>
2024-10-04 16:22:54 -0300

    Adicionando primeiro arquivo SQL

---
+++ SQLQuery4.sql
@@ -0,0 +1,93 @@
+--Questão 1
+
+--Etapa A
+
+--SELECT *
+--FROM DimProduct
+
+--Aquantidade de produtos está correta (2.517).
+
+
+--Etapa B
+
+--SELECT *
+--FROM DimCustomer
+
+--O número de clientes reduziu, no momento existem 18.869 clientes.
+
+
+
+--Questão 2
+
+--Etapa A
+
+--SELECT CustomerKey, FirstName, EmailAddress, BirthDate
+--FROM DimCustomer
+
+
+--Etapa B
+
+--SELECT
+--CustomerKey AS 'ID do Cliente',
+--FirstName AS Nome,
+--EmailAddress AS Email,
+--BirthDate AS 'Data de Nascimento'
+--From DimCustomer
+
+
+
+--Questão 3
+
+--Etapa A
+
+--SELECT TOP(100) *
+--FROM DimCustomer
+
+
+--Etapa B
+
+--SELECT TOP(20) PERCENT *
+--FROM DimCustomer
+
+
+--Etapa C
+
+--SELECT TOP(100) FirstName, EmailAddress, BirthDate
+--FROM DimCustomer
+
+
+--Etapa D
+
+--SELECT
+--FirstName AS Nome,
+--EmailAddress AS Email,
+--BirthDate AS 'Data de Nascimento'
+--From DimCustomer
+
+
+
+--Questão 4 
+
+--SELECT Manufacturer
+--FROM DimProduct
+--SELECT
+--Manufacturer AS Fornecedor
+--FROM DimProduct
+
+
+
+--Questão 5
+
+--SELECT DISTINCT ProductKey
+--FROM FactSales
+
+--2.516 (Produtos vendidos)
+
+
+--SELECT DISTINCT ProductName
+--FROM DimProduct
+
+--2.517 (Produtos existentes)
+
+
+--1 produto nao foi vendido
