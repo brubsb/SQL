264f849b5b9a9669fd3616db966fbb3c7c5f9b4a
Bruna Barboza <165702836+brubsb@users.noreply.github.com>
2024-10-08 15:54:29 -0300

    Descomentando

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -2,16 +2,16 @@
 
 --Etapa A
 
---SELECT *
---FROM DimProduct
+SELECT *
+FROM DimProduct
 
 --Aquantidade de produtos está correta (2.517).
 
 
 --Etapa B
 
---SELECT *
---FROM DimCustomer
+SELECT *
+FROM DimCustomer
 
 --O número de clientes reduziu, no momento existem 18.869 clientes.
 
@@ -21,18 +21,18 @@
 
 --Etapa A
 
---SELECT CustomerKey, FirstName, EmailAddress, BirthDate
---FROM DimCustomer
+SELECT CustomerKey, FirstName, EmailAddress, BirthDate
+FROM DimCustomer
 
 
 --Etapa B
 
---SELECT
---CustomerKey AS 'ID do Cliente',
---FirstName AS Nome,
---EmailAddress AS Email,
---BirthDate AS 'Data de Nascimento'
---From DimCustomer
+SELECT
+CustomerKey AS 'ID do Cliente',
+FirstName AS Nome,
+EmailAddress AS Email,
+BirthDate AS 'Data de Nascimento'
+From DimCustomer
 
 
 
@@ -40,52 +40,52 @@
 
 --Etapa A
 
---SELECT TOP(100) *
---FROM DimCustomer
+SELECT TOP(100) *
+FROM DimCustomer
 
 
 --Etapa B
 
---SELECT TOP(20) PERCENT *
---FROM DimCustomer
+SELECT TOP(20) PERCENT *
+FROM DimCustomer
 
 
 --Etapa C
 
---SELECT TOP(100) FirstName, EmailAddress, BirthDate
---FROM DimCustomer
+SELECT TOP(100) FirstName, EmailAddress, BirthDate
+FROM DimCustomer
 
 
 --Etapa D
 
---SELECT
---FirstName AS Nome,
---EmailAddress AS Email,
---BirthDate AS 'Data de Nascimento'
---From DimCustomer
+SELECT
+FirstName AS Nome,
+EmailAddress AS Email,
+BirthDate AS 'Data de Nascimento'
+From DimCustomer
 
 
 
 --Questão 4 
 
---SELECT DISTINCT Manufacturer
---FROM DimProduct
---SELECT
---Manufacturer AS Fornecedor
---FROM DimProduct
+SELECT DISTINCT Manufacturer
+FROM DimProduct
+SELECT
+Manufacturer AS Fornecedor
+FROM DimProduct
 
 
 
 --Questão 5
 
---SELECT DISTINCT ProductKey
---FROM FactSales
+SELECT DISTINCT ProductKey
+FROM FactSales
 
 --2.516 (Produtos vendidos)
 
 
---SELECT DISTINCT ProductKey
---FROM DimProduct
+SELECT DISTINCT ProductKey
+FROM DimProduct
 
 --2.517 (Produtos existentes)
 
