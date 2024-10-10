d060f4617bd450e678a236bb01e4078caa26cd61
Bruna Barboza <165702836+brubsb@users.noreply.github.com>
2024-10-10 14:34:07 -0300

    Add files via upload

---
+++ Dia 4.sql
@@ -0,0 +1,149 @@
+--Questão 1
+
+--A
+
+SELECT 
+	ChannelKey, 
+	SUM(SalesQuantity) AS 'Qtd. Vendas'
+FROM FactSales
+GROUP BY ChannelKey
+
+--B
+
+SELECT 
+	StoreKey, 
+    SUM(SalesQuantity) AS 'Qtd. Vendas', 
+    SUM(ReturnQuantity) AS 'Qtd. Devolução'
+FROM FactSales
+GROUP BY StoreKey
+
+--C
+
+SELECT 
+	ChannelKey, 
+	SUM(SalesAmount) AS 'Valor das Vendas'
+FROM FactSales
+WHERE YEAR(DateKey) = 2007
+GROUP BY ChannelKey
+
+
+--Questão 2
+
+SELECT TOP(10)
+	ProductKey,
+	SUM(SalesQuantity) AS 'Qtd. Vendas',
+	SUM(SalesAmount) AS 'Valor das Vendas'
+FROM FactSales
+GROUP BY ProductKey
+--HAVING SUM(SalesAmount) > 5000000
+ORDER BY SUM(SalesQuantity) DESC
+
+
+--Questão 3
+
+--A
+
+SELECT TOP(1)
+	CustomerKey,
+	SUM(SalesQuantity) AS 'Qtd. Compras Online'
+FROM FactOnlineSales
+GROUP BY CustomerKey
+ORDER BY SUM(SalesQuantity) DESC
+
+--B
+
+SELECT TOP(3)
+	ProductKey,
+	SUM(SalesQuantity) AS 'Qtd. Compras Online'
+FROM FactOnlineSales
+WHERE CustomerKey = 19037
+GROUP BY ProductKey
+ORDER BY SUM(SalesQuantity) DESC
+
+
+--Questão 4
+
+--A
+
+SELECT DISTINCT
+	BrandName,
+	COUNT(ProductKey) AS 'Qtd. Produtos'
+FROM DimProduct
+GROUP BY BrandName
+
+--B
+
+SELECT DISTINCT
+	ClassName,
+	AVG(UnitPrice) AS 'Preço Unitário'
+FROM DimProduct
+GROUP BY ClassName
+
+--C
+
+SELECT
+	ColorName,
+	SUM(Weight) AS 'Peso Total'
+FROM DimProduct
+GROUP BY ColorName
+
+
+--Questão 5
+
+SELECT
+	StockTypeName,
+	SUM(Weight) AS 'Peso Total da Marca Contoso'
+FROM DimProduct
+WHERE BrandName = 'Contoso'
+GROUP BY StockTypeName
+ORDER BY SUM(Weight) DESC
+
+
+--Questão 6
+
+SELECT
+	BrandName,
+	COUNT(DISTINCT ColorName) AS 'Cores'
+FROM DimProduct
+GROUP BY BrandName
+
+
+--Questão 7
+
+SELECT
+	Gender,
+	COUNT(CustomerKey) AS 'Total de Clienes',
+	AVG(YearlyIncome) AS 'Média Salarial'
+FROM DimCustomer
+WHERE Gender IS NOT NULL
+GROUP BY Gender
+
+
+--Questão 8
+
+SELECT
+	Education,
+	COUNT(CustomerKey) AS 'Total de Clienes',
+	AVG(YearlyIncome) AS 'Média Salarial'
+FROM DimCustomer
+GROUP BY Education
+
+
+--Questão 9
+
+SELECT
+	DepartmentName,
+	COUNT(EmployeeKey) AS 'Total de Funcionários'
+FROM DimEmployee
+WHERE EndDate IS NULL
+GROUP BY DepartmentName
+
+
+--Questão 10
+
+SELECT
+	Title,
+	SUM(VacationHours) AS 'Total de Horas de Férias'
+FROM DimEmployee
+WHERE Gender = 'F' AND DepartmentName IN ('Production', 'Marketing', 'Engineering', 'Finance') AND HireDate BETWEEN '01/01/1999' AND '31/12/2000'
+GROUP BY Title
