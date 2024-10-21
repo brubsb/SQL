61e5cd9ce51b84c1b569da06fc6a32b3274540a4
Bruna Barboza <165702836+brubsb@users.noreply.github.com>
2024-10-21 14:55:12 -0300

    Add files via upload

---
+++ SQL5.sql
@@ -0,0 +1,132 @@
+--Questão 1
+
+SELECT 
+    p.ProductKey, 
+    p.ProductName, 
+    ps.ProductSubcategoryKey, 
+    ps.ProductSubcategoryName
+FROM DimProduct p
+INNER JOIN DimProductSubcategory ps 
+ON p.ProductSubcategoryKey = ps.ProductSubcategoryKey;
+
+
+--Questão 2
+
+SELECT 
+    ps.ProductSubcategoryKey, 
+    ps.ProductSubcategoryName, 
+    ps.ProductCategoryKey, 
+    pc.ProductCategoryName
+FROM DimProductSubcategory ps
+LEFT JOIN DimProductCategory pc 
+ON ps.ProductCategoryKey = pc.ProductCategoryKey;
+
+
+--Questão 3
+
+SELECT 
+    s.StoreKey, 
+    s.StoreName, 
+    s.EmployeeCount, 
+    g.ContinentName, 
+    g.RegionCountryName
+FROM DimStore s
+LEFT JOIN DimGeography g 
+ON s.GeographyKey = g.GeographyKey;
+
+
+--Questão 4
+
+SELECT 
+    p.ProductKey, 
+    p.ProductName, 
+    ps.ProductSubcategoryName, 
+    pc.ProductCategoryName, 
+    pc.ProductCategoryDescription
+FROM DimProduct p
+LEFT JOIN DimProductSubcategory ps 
+ON p.ProductSubcategoryKey = ps.ProductSubcategoryKey
+LEFT JOIN DimProductCategory pc 
+ON ps.ProductCategoryKey = pc.ProductCategoryKey;
+
+
+--Questão 5
+
+SELECT TOP(100) *
+FROM FactStrategyPlan;
+
+SELECT 
+    fsp.StrategyPlanKey, 
+    fsp.DateKey, 
+    da.AccountName, 
+    fsp.Amount
+FROM FactStrategyPlan fsp
+INNER JOIN DimAccount da 
+ON fsp.AccountKey = da.AccountKey;
+
+
+--Questão 6
+
+SELECT 
+    fsp.StrategyPlanKey, 
+    fsp.DateKey, 
+    ds.ScenarioName, 
+    fsp.Amount
+FROM FactStrategyPlan fsp
+INNER JOIN DimScenario ds 
+ON fsp.ScenarioKey = ds.ScenarioKey;
+
+
+--Questão 7
+
+SELECT 
+    ps.ProductSubcategoryKey, 
+    ps.ProductSubcategoryName
+FROM DimProductSubcategory ps
+LEFT JOIN DimProduct p 
+ON ps.ProductSubcategoryKey = p.ProductSubcategoryKey
+WHERE p.ProductKey IS NULL;
+
+
+--Questão 8
+
+SELECT
+BrandName,
+DimChannel.ChannelName
+FROM DimProduct
+INNER JOIN FactSales
+ON FactSales.ProductKey = DimProduct.Productkey
+INNER JOIN DimChannel
+ON DimChannel.ChannelKey = FactSales.ChannelKey
+
+
+--Questão 09
+
+SELECT 
+OnlineSalesKey,
+DateKey,
+PromotionName,
+SalesAmount	
+FROM FactOnlineSales
+INNER JOIN DimPromotion
+ON FactOnlineSales.PromotionKey = DimPromotion.PromotionKey
+WHERE PromotionName <> 'No Discount'
+ORDER BY DateKey ASC
+
+
+--Questão 10
+
+SELECT 
+FactSales.SalesKey,
+DimChannel.ChannelName,
+DimStore.StoreName,
+DimProduct.ProductName,
+FactSales.SalesAmount
+FROM FactSales
+INNER JOIN DimChannel
+ON FactSales.channelKey = DimChannel.ChannelKey
+INNER JOIN DimStore
+ON FactSales.StoreKey = DimStore.StoreKey
+INNER JOIN DimProduct
+ON FactSales.ProductKey = DimProduct.ProductKey
+ORDER BY FactSales.SalesAmount DESC
